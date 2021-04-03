312a819aa96325e5b6dc9a4c6cd3c9fabe924b92
pamela.fracalossi <fracalossiazevedo@gmail.com>
2021-04-03 04:28:05 -0300

    Implemented methods to retrieve lines, bytes and extensions

--- src/main/java/com/trustly/apigithub/controller/WebScraperController.java
+++ src/main/java/com/trustly/apigithub/controller/WebScraperController.java
@@ -1,5 +1,7 @@
 package com.trustly.apigithub.controller;
 
+import com.trustly.apigithub.dto.GitHubDto;
+import com.trustly.apigithub.dto.GitHubRepositorieDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -22,14 +26,15 @@ public class WebScraperController {
 
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> findAll() {
+    public ResponseEntity<List<GitHubDto>> findAll() {
 
-        StringBuilder content = getFiles("https://github.com/angular/protractor/tree/5.4.1");
+        GitHubRepositorieDto repositorieInformation = new GitHubRepositorieDto();
+        getFiles("https://github.com/Prempeh-Gyan/WebScraper", repositorieInformation);
 
-        return ResponseEntity.ok().body(content.toString());
+        return ResponseEntity.ok().body(repositorieInformation.getRepositorieInformation());
     }
 
-    private StringBuilder getFiles(String urlGitHub) {
+    private StringBuilder getFiles(String urlGitHub, GitHubRepositorieDto repositorieInformation) {
         StringBuilder content = new StringBuilder();
         List<String> urlFilesList = new ArrayList<>();
         List<String> urlFolderList = new ArrayList<>();
@@ -51,7 +56,7 @@ public class WebScraperController {
                             urlFolderList.add(getUrl(currentLine.toString()));
                         } else {
                             if (currentLine.toString().contains("/blob/")) {
-                                urlFilesList.add(getUrl(currentLine.toString()));
+                                getInformationFile(getUrl(currentLine.toString()), repositorieInformation);
                             }
                         }
                     }
@@ -67,13 +72,105 @@ public class WebScraperController {
                 StringBuilder urlFolder = new StringBuilder();
                 urlFolder.append("https://github.com");
                 urlFolder.append(folder);
-                    getFiles(urlFolder.toString());
+                    getFiles(urlFolder.toString(), repositorieInformation);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println(urlFilesList);
+        //System.out.println(urlFilesList);
         return content;
+    }
+
+    private void getInformationFile(String partialUrlFile, GitHubRepositorieDto repositorieInformation) {
+        StringBuilder content = new StringBuilder();
+        StringBuilder urlFile = new StringBuilder();
+        urlFile.append("https://github.com");
+        urlFile.append(partialUrlFile);
+        GitHubDto gitHubDto = new GitHubDto();
+
+        try {
+
+            URL url = new URL(urlFile.toString());
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            boolean c = true;
+            while ((line = bufferedReader.readLine()) != null && c) {
+                StringBuilder currentLine = new StringBuilder();
+                currentLine.append(line + "\n");
+                content.append(line + "\n");
+
+                if (content.toString().contains("id=\"blob-path\"") && !content.toString().contains("id=\"raw-url\"")) {
+                    if (currentLine.toString().contains("class=\"final-path\"")) {
+                        gitHubDto.setExtension(getExtention(currentLine.toString()));
+                    }
+                    if (currentLine.toString().contains("lines")) {
+                        gitHubDto.setLines(getLines(currentLine.toString()));
+                    }
+                    if (currentLine.toString().contains("Bytes") || currentLine.toString().contains("KB")) {
+                        gitHubDto.setBytes(getBytes(currentLine.toString()));
+                    }
+                } else {
+                    if (content.toString().contains("id=\"blob-path\"")) {
+                        c = false;
+                    }
+                }
+            }
+            bufferedReader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private Long getBytes(String toString) {
+        Pattern pattern = Pattern.compile("    (\\S+) Bytes");
+        Pattern pattern2 = Pattern.compile("    (\\S+) KB");
+        Matcher matcher = pattern.matcher(toString);
+        Matcher matcher2 = pattern2.matcher(toString);
+        if (matcher.find()) {
+            String result = matcher.group(1);
+            System.out.println(result);
+            return Long.valueOf(result);
+        }else{
+            if (matcher2.find()) {
+                String result = matcher2.group(1);
+                System.out.println(result);
+                NumberFormat format = NumberFormat.getInstance(Locale.US);
+                Number number = 0;
+                try {
+                    number = format.parse(result);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                Double d = number.doubleValue() * 1000;
+                return (d.longValue());
+            }
+        }
+        return 0L;
+    }
+
+    private Long getLines(String toString) {
+        Pattern pattern = Pattern.compile("      (\\S+) lines");
+        Matcher matcher = pattern.matcher(toString);
+        if (matcher.find()) {
+            String result = matcher.group(1);
+            System.out.println(result);
+            return Long.valueOf(result);
+        }
+        return 0L;
+    }
+
+    private String getExtention(String toString) {
+        Pattern pattern = Pattern.compile("class=\"final-path\">(\\S+)</strong>");
+        Matcher matcher = pattern.matcher(toString);
+        if (matcher.find()) {
+            String result = matcher.group(1);
+            System.out.println(result);
+            return result;
+        }
+        return "";
     }
 
 
